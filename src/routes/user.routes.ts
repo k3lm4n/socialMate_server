4640fd0f2a1294fdc588e6046e0482fc3fd2acae
Kelman Dias dos Santos <69762912+k3lm4n@users.noreply.github.com>
2023-06-17 18:12:27 +0100

    Priv

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,24 +1,21 @@
 import { Router, Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
 import userController from "../controllers/user";
-import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
+import { ensureAuthenticated } from "../middleware/EnsureAuthenticated";
 
 const routes = Router();
 
 
-dotenv.config();
 
-routes.get("/", userController.getAll);
+routes.get("/",ensureAuthenticated, userController.getAll);
 
 routes.post("/", userController.register);
 
-routes.get("/:id", userController.getById);
+routes.get("/:id",ensureAuthenticated, userController.getById);
 
-routes.delete("/:id", userController.delete);
+routes.delete("/:id",ensureAuthenticated, userController.delete);
 
-routes.put("/:id", userController.update);
+routes.put("/:id",ensureAuthenticated, userController.update);
 
-routes.get("/search", userController.search);
+routes.get("/search",ensureAuthenticated, userController.search);
 
 export default routes;
