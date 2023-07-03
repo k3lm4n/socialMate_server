5dd231fd4a358c85f8356da4f09045fdf2beaeed
Kelman Dias dos Santos <69762912+k3lm4n@users.noreply.github.com>
2023-07-03 21:38:24 +0100

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -58,8 +58,8 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
