6b596b4a586982235944834ea0234b523007aec6
Kelman Dias dos Santos <69762912+k3lm4n@users.noreply.github.com>
2024-08-14 10:24:47 +0100

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -27,6 +27,7 @@ app.use(
       "http://localhost:3030",
       "https://social.oowl.tech",
       "http://localhost:3000",
+      "https://socialmate.k3lm4n.io/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
